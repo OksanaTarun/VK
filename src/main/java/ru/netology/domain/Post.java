285d93b68871ae16628df080803b8d0ee90502ef
OksanaTarun <oxana.tarun@gmail.com>
2021-03-01 12:33:24 +0300

    updated

--- src/main/java/ru/netology/domain/Post.java
+++ src/main/java/ru/netology/domain/Post.java
@@ -8,14 +8,139 @@ public class Post {
     private String text;
     private int createdDateTime;
     private int createdBy;
-
     private int viewsCount;
-
     private CommentsInfo commentsInfo;
     private RepostsInfo repostsInfo;
     private LikesInfo likesInfo;
-
+    private Geo geo;
+    private String fromID;
+    private boolean isFriendsOnly;
+    private Copyright copyright;
+    private String postType;
+    private String signedId;
+    private boolean canPin;
+    private boolean canDelete;
+    private boolean canEdit;
+    private boolean isPinned;
+    private boolean markedAsAds;
+    private boolean isFavorite;
+    private Donut donut;
+    private boolean postponedId;
     private Attachment[] attachments;
+
+    public Geo getGeo() {
+        return geo;
+    }
+
+    public void setGeo(Geo geo) {
+        this.geo = geo;
+    }
+
+    public String getFromID() {
+        return fromID;
+    }
+
+    public void setFromID(String fromID) {
+        this.fromID = fromID;
+    }
+
+    public boolean isFriendsOnly() {
+        return isFriendsOnly;
+    }
+
+    public void setFriendsOnly(boolean friendsOnly) {
+        isFriendsOnly = friendsOnly;
+    }
+
+    public Copyright getCopyright() {
+        return copyright;
+    }
+
+    public void setCopyright(Copyright copyright) {
+        this.copyright = copyright;
+    }
+
+    public String getPostType() {
+        return postType;
+    }
+
+    public void setPostType(String postType) {
+        this.postType = postType;
+    }
+
+    public String getSignedId() {
+        return signedId;
+    }
+
+    public void setSignedId(String signedId) {
+        this.signedId = signedId;
+    }
+
+    public boolean isCanPin() {
+        return canPin;
+    }
+
+    public void setCanPin(boolean canPin) {
+        this.canPin = canPin;
+    }
+
+    public boolean isCanDelete() {
+        return canDelete;
+    }
+
+    public void setCanDelete(boolean canDelete) {
+        this.canDelete = canDelete;
+    }
+
+    public boolean isCanEdit() {
+        return canEdit;
+    }
+
+    public void setCanEdit(boolean canEdit) {
+        this.canEdit = canEdit;
+    }
+
+    public boolean isPinned() {
+        return isPinned;
+    }
+
+    public void setPinned(boolean pinned) {
+        isPinned = pinned;
+    }
+
+    public boolean isMarkedAsAds() {
+        return markedAsAds;
+    }
+
+    public void setMarkedAsAds(boolean markedAsAds) {
+        this.markedAsAds = markedAsAds;
+    }
+
+    public boolean isFavorite() {
+        return isFavorite;
+    }
+
+    public void setFavorite(boolean favorite) {
+        isFavorite = favorite;
+    }
+
+    public Donut getDonut() {
+        return donut;
+    }
+
+    public void setDonut(Donut donut) {
+        this.donut = donut;
+    }
+
+    public boolean isPostponedId() {
+        return postponedId;
+    }
+
+    public void setPostponedId(boolean postponedId) {
+        this.postponedId = postponedId;
+    }
+
+
 
     public int getId() {
         return id;
